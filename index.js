81441be6568e004c8db1a00a35419a5ef763acb9
Aditya Pradhan <aditya1pradhan21@gmail.com>
2024-08-09 09:18:37 +0530

    syncing the deployment

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-      origin: "https://browser-games-ten.vercel.app/",
+      origin: "https://browser-games-ten.vercel.app",
       methods: ["GET", "POST"],
     },
   });
